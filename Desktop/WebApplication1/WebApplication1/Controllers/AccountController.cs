ba3fda72b0b82e10a256b30367b2d6563b0e1d13
Andreey <andreey.coutinho@ufu.br>
2025-08-29 10:58:22 -0300

    Commit Inicial

---
+++ Desktop/WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace MeuProjetoMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Aqui você salvaria no banco de dados
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Desktop/WebApplication1/WebApplication1/Models/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class RegisterViewModel
+    {
+            [Required(ErrorMessage = "O username é obrigatório")]
+            public string Username { get; set; }
+
+            [Required(ErrorMessage = "A senha é obrigatória")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Required(ErrorMessage = "O email é obrigatório")]
+            [EmailAddress(ErrorMessage = "Email inválido")]
+            public string Email { get; set; }
+        }
+    }
